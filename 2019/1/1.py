eaa5cbb00f20875814153168fac0b0fb49d251c5
David Poblador i Garcia <david@poblador.com>
2019-12-01 06:23:31 +0100

    Solve problem 1

---
+++ 2019/1/1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+filename = 'input.txt'
+
+with open(filename, 'r') as fp:
+    lines = (l.strip() for l in fp)
+
+    s = 0
+
+    for line in lines:
+        i = int(line)
+        d = i / 3
+        e = d - 2
+        s = s + e
+
+
+print(s)
