3da1f8e032f831cbe4cc75ee359d56d46ed09dd4
David Poblador i Garcia <david@poblador.com>
2019-12-02 06:58:26 +0100

    day 2 solution

--- 2019/2/1.py
+++ 2019/2/1.py
@@ -3,9 +3,27 @@
 filename = 'input.txt'
 
 with open(filename, 'r') as fp:
-    lines = (l.strip() for l in fp)
+    line = fp.readline().strip()
 
-    for line in lines:
-        i = int(line)
+v = [int(v) for v in line.split(',')]
 
-        print(line, i)
+v[1] = 12
+v[2] = 2
+
+i = 0
+while i < len(v):
+    c = v[i]
+
+    if c == 99:
+        break
+    elif c == 1:
+        v[v[i+3]] = v[v[i+1]] + v[v[i+2]]
+        i = i + 4
+    elif c == 2:
+        v[v[i+3]] = v[v[i+1]] * v[v[i+2]]
+        i = i + 4
+    else:
+        raise
+
+
+print("OUY", v[0])
--- 2019/2/2.py
+++ 2019/2/2.py
@@ -3,9 +3,34 @@
 filename = 'input.txt'
 
 with open(filename, 'r') as fp:
-    lines = (l.strip() for l in fp)
+    line = fp.readline().strip()
 
-    for line in lines:
-        i = int(line)
+items = [int(v) for v in line.split(',')]
 
-        print(line, i)
+def out(v, noun = 12, verb = 2):
+    v[1] = noun
+    v[2] = verb
+    i = 0
+    while i < len(v):
+        c = v[i]
+
+        if c == 99:
+            break
+        elif c == 1:
+            v[v[i+3]] = v[v[i+1]] + v[v[i+2]]
+            i = i + 4
+        elif c == 2:
+            v[v[i+3]] = v[v[i+1]] * v[v[i+2]]
+            i = i + 4
+        else:
+            raise
+
+    return v[0]
+
+target = 19690720
+
+for n in range(0, 100):
+    for v in range(0, 100):
+        o = out(items.copy(), n, v)
+        if o == target:
+            print((n * 100)+v)
