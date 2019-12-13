616bab5bde5c5e1809d86c57032e5e3f2380ef9d
David Poblador i Garcia <david@poblador.com>
2019-12-13 20:17:43 +0100

    Add skeleton

---
+++ 2019/day-skel.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from pprint import pprint
+from aocd import AocdError
+
+try:
+    from aocd import data, submit
+except AocdError:
+    exit("Something is not right (probably missing day)")
+
+
+def A():
+    pass
+
+
+def B():
+    pass
+
+
+def print_solutions():
+    for part in ['A', 'B']:
+        print("PART {}:".format(part), globals()[part]())
+
+
+if __name__ == "__main__":
+    print_solutions()
