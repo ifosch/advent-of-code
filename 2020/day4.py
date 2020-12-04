749df353f17d089dfe1f2965c31581ae8d27b6af
David Poblador i Garcia <david@poblador.com>
2020-12-04 10:22:03 +0100

    day 4 crappy solution

--- 2020/day4.py
+++ 2020/day4.py
@@ -3,6 +3,8 @@
 from pprint import pprint
 from aocd import AocdError
 
+import re
+
 try:
     from aocd import data, submit
 except AocdError:
@@ -10,27 +12,111 @@ except AocdError:
 
 
 def A():
+
+    passport = {}
     valid = 0
     for line in data.splitlines():
-        tokens = line.split()
+        if line:
+            for i in line.split():
+                k, v = i.split(":")
+                passport[k] = v
+            continue
 
-        min_r, max_r = map(int, tokens[0].split('-'))
-        ch = tokens[1].rstrip(":")
-        pw = tokens[2]
+        if hasallfields(passport):
+            valid += 1
+        
+        passport = {}
 
-        reps = 0
-        for e in pw:
-            if e == ch:
-                reps += 1
-
-        if reps >= min_r and reps <= max_r:
+    else:
+        if hasallfields(passport):
             valid += 1
 
     return valid
 
 
 def B():
-    return 0
+    passport = {}
+    valid = 0
+    for line in data.splitlines():
+        if line:
+            for i in line.split():
+                k, v = i.split(":")
+                passport[k] = v
+            continue
+
+        if hasallfields(passport) and isvalid(passport):
+            valid += 1
+        
+        passport = {}
+
+    else:
+        if hasallfields(passport) and isvalid(passport):
+            valid += 1
+
+    return valid
+
+
+def isvalid(passport):
+    for k, v in passport.items():
+        if k == 'byr':
+            if not len(v) == 4:
+                return False
+            if int(v) < 1920 or int(v) > 2002:
+                return False
+        elif k == 'iyr':
+            if not len(v) == 4:
+                return False
+            if int(v) < 2010 or int(v) > 2020:
+                return False
+        elif k == 'eyr':
+            if not len(v) == 4:
+                return False
+            if int(v) < 2020 or int(v) > 2030:
+                return False
+        elif k == 'hgt':
+            if v.endswith('in'):
+                h = int(v[:-2])
+                if h < 59 or h > 76:
+                    return False
+            elif v.endswith('cm'):
+                h = int(v[:-2])
+                if h < 150 or h > 193:
+                    return False
+            else:
+                return False
+        elif k == "hcl":
+            if not re.match(r"^#[0-9a-f]{6}$", v):
+                return False
+        elif k == "ecl":
+            if v not in ('amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'):
+                return False
+        elif k == "pid":
+            if not re.match(r"^[0-9]{9}$", v):
+                return False
+
+    return True
+
+
+def hasallfields(passport):
+    fields = (
+        'byr',  # (Birth Year)
+        'iyr',  # (Issue Year)
+        'eyr',  # (Expiration Year)
+        'hgt',  # (Height)
+        'hcl',  # (Hair Color)
+        'ecl',  # (Eye Color)
+        'pid',  # (Passport ID)
+        'cid',  # (Country ID)
+    )
+
+    for f in fields:
+        if f == 'cid':
+            continue
+
+        if f not in passport:
+            return False
+
+    return True
 
 
 def print_solutions():
